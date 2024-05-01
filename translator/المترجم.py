f327b1002ac4dd30941dd71180e56ac5d6d2e91a
abd alrhman mohamed gad alrab <162161775+MesterAbdAlrhmanMohmed@users.noreply.github.com>
2024-05-01 07:20:22 +0300

    This program translates from any language to any language
    This program translates from any language to any language

--- translator/المترجم.py
+++ translator/المترجم.py
@@ -2,7 +2,9 @@ from PyQt6 import QtWidgets as qt
 from PyQt6 import QtGui as qt1
 from PyQt6 import QtCore as qt2
 from translator import *
-import dic,about,pyperclip
+import dic,about,pyperclip,os,langdetect,gtts
+if not os.path.exists("data"):
+        os.makedirs("data")
 class main (qt.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -24,6 +26,9 @@ class main (qt.QMainWindow):
         self.نسخ=qt.QPushButton("نسخ الترجمة")
         self.نسخ.setDefault(True)
         self.نسخ.clicked.connect(self.copy)
+        self.إستماع=qt.QPushButton("الاستماع الى الترجمة")
+        self.إستماع.setDefault(True)
+        self.إستماع.clicked.connect(self.listen)
         self.عن=qt.QPushButton("عن المطور")
         self.عن.setDefault(True)
         self.عن.clicked.connect(self.about)
@@ -36,6 +41,7 @@ class main (qt.QMainWindow):
         l.addWidget(self.إظهار3)
         l.addWidget(self.الترجمة_هي)
         l.addWidget(self.نسخ)
+        l.addWidget(self.إستماع)
         l.addWidget(self.عن)
         w=qt.QWidget()
         w.setLayout(l)
@@ -51,6 +57,15 @@ class main (qt.QMainWindow):
             qt.QMessageBox.warning(self,"تنبيه","فشلت عملية الترجمة, ربما اللغة غير مدعومة, أو هناك خطأ في الإنترنت")
     def copy(self):
         pyperclip.copy(self.الترجمة_هي.text())
+    def listen(self):
+        try:
+            النص=self.الترجمة_هي.text()
+            lang=langdetect.detect(النص)
+            result=gtts.gTTS(النص,lang=lang)
+            result.save("data/message.mp3")
+            os.startfile(os.getcwd()+"/data/message.mp3")
+        except:
+            qt.QMessageBox.information(self,"تنبيه","عفوا, يرجى إدخال نص أو التأكد من الإتصال بالإنترنت")
 app=qt.QApplication([])
 app.setStyle('fusion')
 w=main()
